9c7b9d238c891715094d0877602a7376fb256a38
Felix Canberger <felix.canberger@hotmail.com>
2022-03-22 14:58:31 +0100

    small fixes

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ require("./mongoose.js");
 const express = require("express");
 const exphbs = require("express-handlebars");
 
-<<<<<<< HEAD
 const registerRoute = require("./routers/registerRoute.js")
-=======
-const userRoute = require("./models/UsersModel.js");
->>>>>>> 3a659a1c71ef51648e97c7b9b06bb1688a0bfd6f
 
 const utils = require("./utils.js");
 
@@ -27,11 +23,7 @@ app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-<<<<<<< HEAD
 app.use('/user', registerRoute);
-=======
-app.use("/user", userRoute);
->>>>>>> 3a659a1c71ef51648e97c7b9b06bb1688a0bfd6f
 
 app.get("/", (req, res) => {
   res.render("home");
