16e190ce09c1083be57ed711dfee8ba9be2791f2
Emma <91128612+EmmaNGagen@users.noreply.github.com>
2022-03-22 13:33:28 +0100

    commit

--- index.js
+++ index.js
@@ -4,28 +4,31 @@ require("./mongoose.js");
 const express = require("express");
 const exphbs = require("express-handlebars");
 
-const userRoute = require("./models/UsersModel.js")
+const userRoute = require("./models/UsersModel.js");
 
 const utils = require("./utils.js");
 
 const app = express();
 
-app.engine("hbs", exphbs.engine({
+app.engine(
+  "hbs",
+  exphbs.engine({
     defaultLayout: "main",
-    extname: ".hbs"
-}));
+    extname: ".hbs",
+  })
+);
 
 app.set("view engine", "hbs");
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use('/', userRoute);
+app.use("/user", userRoute);
 
 app.get("/", (req, res) => {
-    res.render("home");
+  res.render("home");
 });
 
 app.listen(8000, () => {
-    console.log("http://localhost:8000");
+  console.log("http://localhost:8000");
 });
