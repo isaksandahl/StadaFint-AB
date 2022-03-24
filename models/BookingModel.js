36904624f285238da0e1c5fd59a7ad0e86a1f367
Simon Sandahl <sandahl.simon@hotmail.se>
2022-03-24 12:43:29 +0100

    functionality to add new booking

--- models/BookingModel.js
+++ models/BookingModel.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
+    firstname: { type: String, required: true},
+    lastname: { type: String, required: true},
+    email: { type: String, required: true},
+    phone: { type: Number, required: true},
+    adress: { type: String, required: true},
+    zip_code: { type: Number, required: true},
     date: { type: String, required: true },
     time: { type: String, required: true},
     type_of_customer: { type: String, required: true},
--- routers/bookingRoute.js
+++ routers/bookingRoute.js
@@ -8,4 +8,38 @@ router.get("/", async (req, res) => {
 
 });
 
+router.post("/new", async (req, res) => {
+    const { 
+        firstname,
+        lastname,
+        email,
+        phone,
+        adress,
+        zip_code,
+        date,
+        time,
+        type_of_service,
+        type_of_customer
+    } = req.body;
+
+    const newBooking = new BookingModel({
+        firstname,
+        lastname,
+        email,
+        phone,
+        adress,
+        zip_code,
+        date,
+        time,
+        type_of_customer,
+        type_of_service
+    });
+
+    await newBooking.save();
+
+    console.log(newBooking);
+
+    res.redirect("/");
+});
+
 module.exports = router;
